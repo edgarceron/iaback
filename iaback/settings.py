3816d27109db53e131eeb60b51cf58adfd246edf
Edgar Ceron <edgar.ceron@coxautoinc.com>
2022-07-13 16:50:04 -0500

    Connection to amazon s3 completed

--- iaback/settings.py
+++ iaback/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AZURE_SEARCH_KEY = os.getenv('AZURE_SEARCH_KEY', '')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'iaback.custom_storage.MediaStorage'
 AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID', '')
 AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
